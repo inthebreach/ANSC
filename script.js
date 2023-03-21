477c07596627ed8b6ba5703b9b90222307b284f1
Jared Leonguerrero <linuxpir8@gmail.com>
2023-03-20 21:06:11 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,19 +5,21 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Define your progress stages and their corresponding percentages
   var progressStages = [
-    { status: "Discovery/Kick-Off Meeting", percentage: 20 },
-  { status: "Content Review", percentage: 40 },
-  { status: "Content Revise", percentage: 60 },
-  { status: "Tech Review", percentage: 80 },
-  { status: "Launched", percentage: 100 },
+    { status: "Discovery/Kick-Off Meeting", percentage: 20, color: "red" },
+  { status: "Content Review", percentage: 40, color: "orange" },
+  { status: "Content Revise", percentage: 60, color: "yellow" },
+  { status: "Tech Review", percentage: 80, color: "blue" },
+  { status: "Launched", percentage: 100, color: "green" },
 ];
 
   // Set the current progress stage (0-4)
   var currentStage = 2;
 
   // Update the progress bar and status text
-  progressElement.style.width = progressStages[currentStage].percentage + "%";
-  statusTextElement.textContent = progressStages[currentStage].status;
+progressElement.style.width = progressStages[currentStage].percentage + "%";
+progressElement.style.backgroundColor = progressStages[currentStage].color;
+statusTextElement.textContent = progressStages[currentStage].status;
+
 
   // Set the last update date
   var date = new Date();
