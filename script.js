319a60eb8286988163c00f2e729a5ba957be302c
Jared Leonguerrero <linuxpir8@gmail.com>
2023-03-20 20:30:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
-const progressElement = document.getElementById("progress");
-const statusTextElement = document.getElementById("statusText");
-const lastUpdatedElement = document.getElementById("lastUpdated");
+<script>
+  function initProgressTracker() {
+    const progressElement = document.getElementById("progress");
+    const statusTextElement = document.getElementById("statusText");
+    const lastUpdatedElement = document.getElementById("lastUpdated");
 
-// Define your progress stages and their corresponding percentages
-const progressStages = [
-  { status: "Discovery/Kick-Off Meeting", percentage: 20 },
-  { status: "Content Review", percentage: 40 },
-  { status: "Content Revise", percentage: 60 },
-  { status: "Tech Review", percentage: 80 },
-  { status: "Launched", percentage: 100 },
-];
+    // Define your progress stages and their corresponding percentages
+    const progressStages = [
+      { status: "Ordered", percentage: 20 },
+      { status: "Preparing", percentage: 40 },
+      { status: "Baking", percentage: 60 },
+      { status: "Out for Delivery", percentage: 80 },
+      { status: "Delivered", percentage: 100 },
+    ];
 
-// Set the current progress stage (0-4)
-const currentStage = 0;
+    // Set the current progress stage (0-4)
+    const currentStage = 2;
 
-// Update the progress bar and status text
-progressElement.style.width = progressStages[currentStage].percentage + "%";
-statusTextElement.textContent = progressStages[currentStage].status;
+    // Update the progress bar and status text
+    progressElement.style.width = progressStages[currentStage].percentage + "%";
+    statusTextElement.textContent = progressStages[currentStage].status;
 
-// Set the last update date
-const date = new Date();
-lastUpdatedElement.textContent = "Last updated: " + date.toLocaleString();
+    // Set the last update date
+    const date = new Date();
+    lastUpdatedElement.textContent = "Last updated: " + date.toLocaleString();
+  }
+
+  document.addEventListener("DOMContentLoaded", initProgressTracker);
+</script>
